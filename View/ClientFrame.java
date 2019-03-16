868d95219a43e6bda7b76bbf34f371899d129efa
Ahmed Soror <ahmedsoror438@gmail.com>
2019-03-17 01:56:31 +0200

    Add files via upload

---
+++ View/ClientFrame.java
@@ -0,0 +1,120 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+
+public class ClientFrame extends JFrame {
+
+	public JPanel contentPane;
+	public JTextField txt_msg;
+	public JTextField txt_port;
+	public JButton btn_send; 
+	public JButton btn_port ;
+	public JList list_members ;
+	public JButton btn_getMembers; 
+	public JTextArea txt_display;
+	
+	/**
+	 * Launch the application.
+	 */
+	public void run() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientFrame frame = new ClientFrame();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	public static void main(String[] args) {
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ClientFrame() {
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 582, 527);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		txt_msg = new JTextField();
+		txt_msg.setColumns(10);
+		
+		btn_send = new JButton("Join");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		txt_port = new JTextField();
+		txt_port.setColumns(10);
+		
+		btn_port = new JButton("Port");
+		
+		list_members = new JList();
+		
+		btn_getMembers = new JButton("Get members");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(txt_port, GroupLayout.PREFERRED_SIZE, 124, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btn_port))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+								.addComponent(txt_msg, GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE))
+							.addGap(18)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(list_members, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btn_getMembers, GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE)
+								.addComponent(btn_send, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 118, Short.MAX_VALUE))))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txt_port, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn_port))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 371, Short.MAX_VALUE)
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addComponent(list_members, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btn_getMembers)))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txt_msg, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn_send))
+					.addContainerGap())
+		);
+		
+		txt_display = new JTextArea();
+		txt_display.setEditable(false);
+		scrollPane.setViewportView(txt_display);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
