e7589c486df2912cfa1d5a3f4ef579a904306989
Ahmed Soror <ahmedsoror438@gmail.com>
2019-03-17 16:33:41 +0200

    Update ServerModel5000.java

--- Model/ServerModel5000.java
+++ Model/ServerModel5000.java
@@ -25,9 +25,8 @@ public class ServerModel5000{
 	static ArrayList<ThreadedServer> sockestList;
 	static Socket server3000;
 	static ClientModel toOtherServer;
-//	static PrintWriter outToServer;
-//	static BufferedReader inFromServer;
-	
+	static boolean server3000Connected;
+
 	public ServerModel5000(int port) throws IOException {
 		this.port=port;
 		ServerSocket=new ServerSocket(port);
@@ -35,9 +34,7 @@ public class ServerModel5000{
 		clientsName=new TreeSet<String>();
 		sockestList=new ArrayList<ServerModel5000.ThreadedServer>();
 		
-//		server3000=new Socket("LocalHost",3000);
 		connectToOtherServer();
-//		inFromServer=new BufferedReader(new InputStreamReader(server3000.getInputStream()));
 		
 	}
 	
@@ -69,6 +66,8 @@ public class ServerModel5000{
 	public static void connectToOtherServer() throws IOException {
 		 toOtherServer = new ClientModel(3000);
 		toOtherServer.sendMessage("$$Server2$$");
+		server3000Connected=true;
+
 	}
 	public static void main(String[] args) throws Exception {
 		int port=5000;
@@ -79,8 +78,180 @@ public class ServerModel5000{
 		catch(Exception e) {
 		}
 	}
-
-
+/*
+	static class ThreadedServer implements Runnable {
+		private Socket socket;
+		private int clientNumber;
+		String clientName="Guest";
+		BufferedReader inFromClient ;
+		PrintWriter outToClient; 
+		boolean firstConnection=true;  // if it is the first time to connect then client is sending his name
+		
+		
+		
+		public ThreadedServer(Socket socket, int clientNumber) throws IOException {
+			this.socket = socket;
+			this.clientNumber = clientNumber;
+			inFromClient= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			outToClient= new PrintWriter(socket.getOutputStream(), true);
+			System.out.println("New client #" + clientNumber + "connected at " + socket);
+		}
+		public void sendToClient(String msg) {
+			outToClient.println(msg);
+		}
+		public void sendToClient(Socket connectionSocket,String msg) throws IOException {
+			PrintWriter pw=new PrintWriter(connectionSocket.getOutputStream(),true);
+			pw.println(msg);
+		}
+		public String readFromClient() throws IOException {
+			String msgIn=inFromClient.readLine();
+			return msgIn;
+		}
+		public String getMemberOtherServer() {
+			String s="";
+			if(server3000Connected) {
+				try {
+					toOtherServer.sendMessage("GetServerList");
+					s=toOtherServer.readMessage();
+					System.out.println("S5000 116 : "+s);
+					s=toOtherServer.readMessage();
+					System.out.println("S5000 118 : "+s);
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			return ","+s;
+		}
+		public String getMemberList() {	
+//			String s=getMemberOtherServer();
+//			return clientsName.toString()+s;
+			return clientsName.toString();
+		}
+		public String getServerList() {	
+			return clientsName.toString();
+		}
+		public String recieverName(String clientSentence) {
+			String name=null;
+			if(clientSentence.charAt(0)=='~') { // ~ name ~
+				name="";
+				for(int i=1;i<clientSentence.length()&&clientSentence.charAt(i)!='~';i++) {
+					name+=clientSentence.charAt(i);
+				}
+			}
+			return name.trim();
+		}
+		
+		
+		public void run() {
+			try {
+				while(true) {
+					String msgIn=readFromClient();
+					System.out.println("s3000 143: "+msgIn);	
+					
+					if(msgIn.equalsIgnoreCase("bye")||msgIn.equalsIgnoreCase("quit")) {
+						break;
+					}
+					if(firstConnection) {
+						clientName=msgIn;
+						if(clientsName.add(msgIn)) {
+							sockestList.add(this);
+							firstConnection=false;
+							if (msgIn.equalsIgnoreCase("$$Server2$$")) {
+								ServerModel3000.connectToOtherServer();
+							}
+							sendToClient("true,"+clientName);
+						}
+						else {
+							sendToClient("name exists");
+						}
+					}
+					else {
+						if(msgIn.equalsIgnoreCase("GetMemberList")||(msgIn.toUpperCase().contains("MEMBER")&&msgIn.toUpperCase().contains("LIST"))) {
+							sendToClient(getMemberList()+"\n");
+						}
+						if(msgIn.equalsIgnoreCase("GetServerList")) {
+							sendToClient(getServerList()+"\n");
+						}
+						else {
+							String toClient=recieverName(msgIn);
+							 if(toClient!=null) {
+								//////////////////////////////////////////////////////			send to client
+								if(msgIn.charAt(msgIn.length()-1)=='0') {
+									System.out.println("Client not found");
+								}
+								else {
+									if(clientsName.contains(toClient)) {
+										for(ThreadedServer se:sockestList) {
+											if(se.clientName.equals(toClient)) {
+												se.sendToClient(msgIn);
+												break;
+											}
+										}
+									}
+									else {
+										int timelive=Integer.parseInt(msgIn.charAt(msgIn.length()-1)+"")-1;
+										String newMsg=msgIn.substring(0,msgIn.length()-1)+""+timelive;
+										ServerModel3000.toOtherServer.sendMessage(newMsg);
+//										outToServer.println(newMsg);
+									}
+								}
+							
+							
+							///////////////////////////////////////////////////
+							}
+							else {
+								sendToClient(msgIn.toUpperCase());
+//								outToServer.println(msgIn);
+							}
+						}
+					}
+					
+				}
+			} catch (Exception e) {
+//				System.out.println(clientNumber+" Closed the connection");
+			} finally {
+				try {
+					socket.close();
+				} 
+				catch (IOException e) {
+				}
+				System.out.println("Connection with client  " + clientName + " closed");
+				clientsName.remove(clientName);
+				for(ThreadedServer se:sockestList) {
+					if(se.clientName.equals(clientName)) {
+						sockestList.remove(se);
+						break;
+					}
+				}
+			}
+		}
+		public void myrun() {
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				out.println(in.readLine().toUpperCase());
+			} catch (Exception e) {
+				System.out.println("Error handling client " + clientName);
+			} finally {
+				try {
+					socket.close();
+				} catch (Exception e) {
+				}
+				System.out.println("Connection with client  " + clientName + " closed 217");
+			    clientsName.remove(clientName);
+				for(ThreadedServer se:sockestList) {
+					if(se.clientName.equals(clientName)) {
+						sockestList.remove(se);
+						break;
+					}
+				}
+			}
+		}
+	}
+*/
 	static class ThreadedServer implements Runnable {
 		private Socket socket;
 		private int clientNumber;
@@ -104,6 +275,24 @@ public class ServerModel5000{
 		public String readFromClient() throws IOException {
 			String msgIn=inFromClient.readLine();
 			return msgIn;
+		}
+		public String getMemberOtherServer() {
+			String s="";
+			if(server3000Connected) {
+				try {
+					toOtherServer.sendMessage("GetMemberList");
+					s=toOtherServer.readMessage();
+					System.out.println("S5000 113 : "+s);
+					s=toOtherServer.readMessage();
+					System.out.println("S5000 115 : "+s);
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			return ","+s;
 		}
 		public String getMemberList() {	
 			return clientsName.toString();
@@ -139,8 +328,6 @@ public class ServerModel5000{
 					}
 					else {
 						if(msgIn.equalsIgnoreCase("GetMemberList")||(msgIn.toUpperCase().contains("MEMBER")&&msgIn.toUpperCase().contains("LIST"))) {
-//							outToServer.println(msgIn);
-//							String list=inFromServer.readLine();
 							sendToClient(getMemberList()+"\n");
 						}
 						else {
@@ -221,5 +408,4 @@ public class ServerModel5000{
 			}
 		}
 	}
-
 }
